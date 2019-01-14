6d8099c70d227b64c09cd35f1de14b74b5c04a63
Enkelmann <nils-edvin.enkelmann@fkie.fraunhofer.de>
2019-01-14 12:54:51 +0100

    Add cross compilation targets (except x86) for C++

--- test/artificial_samples/install_cross_compilers_ubuntu_18.04.sh
+++ test/artificial_samples/install_cross_compilers_ubuntu_18.04.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "Installing cross compiler for ARM architecture."
-sudo apt install -y gcc-multilib-arm-linux-gnueabi
+sudo apt install -y gcc-multilib-arm-linux-gnueabi g++-arm-linux-gnueabi
 echo "Installing cross compiler for MIPS architecture."
-sudo apt install -y gcc-multilib-mips-linux-gnu
+sudo apt install -y gcc-multilib-mips-linux-gnu g++-7-mips-linux-gnu
 echo "Installing cross compiler for PPC architecture."
-sudo apt install -y gcc-multilib-powerpc-linux-gnu
+sudo apt install -y gcc-multilib-powerpc-linux-gnu g++-7-powerpc-linux-gnu
 echo "Done."
