7006f3c71ce6b78f48c2f86f06973c04596a110f
Enkelmann <nils-edvin.enkelmann@fkie.fraunhofer.de>
2019-01-21 11:18:13 +0100

    Better test for CWE-248

--- src/checkers/cwe_248.ml
+++ src/checkers/cwe_248.ml
@@ -4,38 +4,86 @@ open Core_kernel.Std
 let name = "CWE248"
 let version = "0.1"
 
-(* filters all jump instructions for exception throw calls that have no return address (according to bap).
-   ToDo: This produces too many false positives right now. *)
-let find_exceptions block =
-  let jmp_instructions = Term.enum jmp_t block in
-    Seq.filter_map jmp_instructions ~f:(fun instr ->
-      match Jmp.kind instr with
-        | Goto _ | Ret _ | Int (_,_) -> None
-        | Call dst -> match Call.target dst with
-          | Direct tid ->
-            (* Tid.name tid is the name of the extern symbol. We are interested in the symbols @__cxa_throw and
-              and @__cxa_allocate_exception. Term.name instr contains the address of the instruction.*)
-            if Tid.name tid = "@__cxa_throw" && dst = Call.with_noreturn dst then
-              Some (Term.tid block)
-            else
-              None
-          | _ -> None)
+(* Print the findings to the log *)
+let print_uncatched_exception block_tid ~tid_map =
+   Log_utils.warn
+     "[%s] {%s} (Possibly Uncaught Exception) (Exception thrown at %s)."
+     name
+     version
+     (Address_translation.translate_tid_to_assembler_address_string block_tid tid_map)
 
-let print_calls calls ~tid_map =
-   Seq.iter calls ~f:(fun call -> match call with
-      | block_tid -> Log_utils.warn
-                       "[%s] {%s} (Possibly Uncaught Exception) (Exception thrown at %s)."
-                       name
-                       version
-                       (Address_translation.translate_tid_to_assembler_address_string block_tid tid_map))
+
+(* Returns the symbol name if the block contains a direct jump to a symbol *)
+let extract_direct_call_tid block =
+  let jmp_instructions = Term.enum jmp_t block in
+  Seq.fold jmp_instructions ~init:None ~f:(fun already_found instr ->
+    match already_found with
+      | Some(symb) -> Some(symb)
+      | None ->
+        match Jmp.kind instr with
+          | Goto _ | Ret _ | Int (_,_) -> None
+          | Call dst -> match Call.target dst with
+            | Direct tid ->
+              (* Tid.name tid is the name of the symbol.*)
+              Some(tid)
+            | _ -> None)
+
+(* Extract the Tid of a direct call, if the block contains a direct call. *)
+let extract_direct_call_symbol block =
+  match extract_direct_call_tid block with
+    | Some(tid) -> Some(Tid.name tid)
+    | None -> None
+
+(* check whether block contains a direct call to a symbol with name symbol_name *)
+let contains_symbol block symbol_name =
+  match extract_direct_call_symbol block with
+    | Some(symb) -> symb = symbol_name
+    | None -> false
+
+  (* Checks whether a subfunction contains a catch block. *)
+  let contains_catch subfunction =
+    let blocks = Term.enum blk_t subfunction in
+    Seq.exists blocks (fun block -> contains_symbol block "@__cxa_begin_catch")
+
+(* Find all calls to subfunctions that are reachable from this subfunction. The calls are returned
+    as a list, except for calls to "@__cxa_throw", which are logged as possibly uncaught exceptions. *)
+let find_calls_and_throws subfunction ~tid_map =
+  let blocks = Term.enum blk_t subfunction in
+  Seq.fold blocks ~init:[] ~f:(fun call_list block ->
+    if contains_symbol block "@__cxa_throw" then
+      let () = print_uncatched_exception  (Term.tid block) ~tid_map:tid_map in
+      call_list
+    else
+      match extract_direct_call_tid block with
+        | Some(tid) -> tid :: call_list
+        | None -> call_list
+  )
+
+(* find exception throws with for which an exception handler was not necessarily allocated beforehand.
+    The return value is a list of all already checked functions.*)
+let rec find_uncaught_exceptions subfunction already_checked_functions program ~tid_map =
+  if contains_catch subfunction then
+    (* This function contains a catch so we assume every throw reachable from here is catched. *)
+    already_checked_functions
+  else
+    let subfunction_calls = find_calls_and_throws subfunction ~tid_map:tid_map in
+    List.fold subfunction_calls ~init:already_checked_functions ~f:(fun already_checked subfunc ->
+      match List.exists ~f:(fun a -> a = subfunc) already_checked with
+        | true -> already_checked
+        | false ->  find_uncaught_exceptions ~tid_map:tid_map (Core_kernel.Option.value_exn (Term.find sub_t program subfunc)) (subfunc :: already_checked) program)
 
 
 let check_cwe program tid_map =
   (* Get all subfunctions *)
   let subfunctions = Term.enum sub_t program in
-  (* Get all blocks of code *)
-  let blocks = Seq.concat_map subfunctions ~f:(fun subfunction -> Term.enum blk_t subfunction) in
-  (* Filter for possibly uncaught exception throws *)
-  let exception_throws = Seq.concat_map blocks ~f:(fun block -> find_exceptions block) in
-  (* print *)
-  print_calls exception_throws ~tid_map:tid_map
+  (* collect all entry points of the program *)
+  let entry_points = Seq.filter subfunctions ~f:(fun subfn -> Term.has_attr subfn Sub.entry_point) in
+  (* TODO: The _start entry point calls a libc-function which then calls the main function. Since right now only direct
+      calls are tracked, our graph traversal never finds the main function. For now, we add it by hand to the entry points.*)
+  let main_fn = Seq.filter subfunctions ~f:(fun subfn -> "@main" = Tid.name (Term.tid subfn)) in
+  let entry_points_with_main = Seq.append main_fn entry_points in
+  (* search for uncatched exceptions for each entry point, but accumulate the list of already checked functions. *)
+  (* TODO: Exceptions, that are catched when starting from one entry point, but not from another, are masked this
+      way. We should check whether this produces a lot of false negatives. *)
+  let _ = Seq.fold entry_points_with_main ~init:[] ~f:(fun already_checked_functions sub -> find_uncaught_exceptions ~tid_map:tid_map sub already_checked_functions program) in
+  ()
--- src/checkers/cwe_248.mli
+++ src/checkers/cwe_248.mli
@@ -1,6 +1,10 @@
 (** This module implements a check for CWE-248 (Uncaught Exception)
 An uncaught exception may lead to a crash and subsequentially to other unintended behavior.
-See https://cwe.mitre.org/data/definitions/248.html for detailed description. *)
+See https://cwe.mitre.org/data/definitions/248.html for detailed description.
+
+Right now we search for exception throws that are reachable in the callgraph without
+touching a function that contains a catch block. We do not check whether a catch block
+can actually catch the thrown exceptions, thus we generate some false negatives. **)
 val name : string
 val version : string
 
