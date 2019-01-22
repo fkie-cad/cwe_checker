e6937fe0c15a58855bf39789deffac3a0992718f
Thomas Barabosch <thomas.barabosch@fkie.fraunhofer.de>
2019-01-22 09:31:09 +0100

    Added acceptance tests for a bunch of checks. Added new test case for cwe676.

---
+++ test/acceptance/test_cwe190.py
@@ -0,0 +1,35 @@
+import unittest
+import cwe_checker_testlib
+
+class TestCwe190(unittest.TestCase):
+
+    def setUp(self):
+        self.target = '190'
+        self.string = b'Integer Overflow or Wraparound'
+
+    def test_cwe190_01_arm(self):
+        expect_res = 3
+        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'arm', self.string)
+        assert res == expect_res
+
+    def test_cwe190_01_x86(self):
+        expect_res = 3
+        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'x86', self.string)
+        assert res == expect_res
+
+    def test_cwe190_01_x64(self):
+        expect_res = 3
+        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'x64', self.string)
+        assert res == expect_res
+
+    @unittest.skip("Depends on proper MIPS support in BAP")
+    def test_cwe190_01_mips(self):
+        expect_res = 3
+        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'mips', self.string)
+        assert res == expect_res
+
+    def test_cwe190_01_ppc(self):
+        expect_res = 3
+        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'ppc', self.string)
+        assert res == expect_res
+    
---
+++ test/acceptance/test_cwe782.py
@@ -0,0 +1,14 @@
+import unittest
+import cwe_checker_testlib
+
+class TestCwe782(unittest.TestCase):
+
+    def setUp(self):
+        self.target = '782'
+        self.string = b'Exposed IOCTL with Insufficient Access Control'
+
+    def test_cwe782_01_x64(self):
+        expect_res = 1
+        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'x64', self.string)
+        assert res == expect_res
+
