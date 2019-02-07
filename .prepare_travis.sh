f91d7941a6d8698af4f8828ec5b06958e1c823ab
Thomas Barabosch <thomas.barabosch@fkie.fraunhofer.de>
2019-02-07 13:46:47 +0100

    Refactored travis ci scripts.

--- .prepare_travis.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Building docker container"
-docker build -t cwe-checker .
--- .travis_prepare.sh
+++ .travis_prepare.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+#!/bin/bash
 cd test/artificial_samples/
 ./install_cross_compilers.sh
 make
+cd ../..
+docker build -t cwe-checker .
