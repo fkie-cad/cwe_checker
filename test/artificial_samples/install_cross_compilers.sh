0bfdb016d9a8b506a9dd9cd0ca72345f61fa60f8
Thomas Barabosch <thomas.barabosch@gmail.com>
2019-02-08 16:27:56 +0100

    More dependencies for x86

--- test/artificial_samples/install_cross_compilers.sh
+++ test/artificial_samples/install_cross_compilers.sh
@@ -6,5 +6,5 @@ sudo apt install -y gcc-multilib-mips-linux-gnu g++-5-mips-linux-gnu
 echo "Installing cross compiler for PPC architecture."
 sudo apt install -y gcc-multilib-powerpc-linux-gnu g++-5-powerpc-linux-gnu
 echo "Installing dependencies for x86 compilation"
-sudo apt install -y libc6-dev-i386
+sudo apt install -y libc6-dev-i386 g++-multilib gcc-multilib
 echo "Done."
