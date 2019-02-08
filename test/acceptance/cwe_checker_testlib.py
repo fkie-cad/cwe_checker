11bff7afd2ff35a9de4f72b7de41ebd3507c2301
Thomas Barabosch <thomas.barabosch@gmail.com>
2019-02-08 12:24:52 +0100

    Use absolute pathes in Travis CI

--- test/acceptance/cwe_checker_testlib.py
+++ test/acceptance/cwe_checker_testlib.py
@@ -3,7 +3,8 @@ import subprocess
 
 def build_bap_cmd(filename, target, arch):
         if 'travis' in os.environ['USER']:
-                cmd = 'docker run --rm -v test/artificial_samples/build/cwe_%s_%s.out:/tmp/input cwe-checker:latest bap /tmp/input --pass=cwe-checker --cwe-checker-partial=CWE%s  --cwe-checker-config=/home/bap/cwe_checker/src/config.json' % (filename, arch, target)
+                abs_path = os.path.abspath('test/artificial_samples/build/cwe_%s_%s.out' % (filename, arch))
+                cmd = 'docker run --rm -v %s:/tmp/input cwe-checker:latest bap /tmp/input --pass=cwe-checker --cwe-checker-partial=CWE%s  --cwe-checker-config=/home/bap/cwe_checker/src/config.json' % (abs_path, target)
         else:
                 cmd = 'bap test/artificial_samples/build/cwe_%s_%s.out  --pass=cwe-checker --cwe-checker-partial=CWE%s --cwe-checker-config=src/config.json' % (filename, arch, target)
         return cmd.split()
