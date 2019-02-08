f5d33e2eccf985205780ed67d342393c1f8af521
Thomas Barabosch <thomas.barabosch@gmail.com>
2019-02-08 14:41:41 +0100

    Build test cases (almost) without dockcross

--- test/artificial_samples/install_cross_compilers.sh
+++ test/artificial_samples/install_cross_compilers.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 echo "Installing cross compiler for ARM architecture."
-docker pull dockcross/linux-armv7
-docker run --rm dockcross/linux-armv7 > ./dockcross-linux-armv7
-chmod +x ./dockcross-linux-armv7
+sudo apt install -y gcc-multilib-arm-linux-gnueabi g++-arm-linux-gnueabi
 echo "Installing cross compiler for MIPS architecture."
-docker pull dockcross/linux-mips
-docker run --rm dockcross/linux-mips > ./dockcross-linux-mips
-chmod +x ./dockcross-linux-mips
+sudo apt install -y gcc-multilib-mips-linux-gnu g++-7-mips-linux-gnu
 echo "Installing cross compiler for PPC architecture."
-docker pull dockcross/linux-ppc64le
-docker run --rm dockcross/linux-ppc64le > ./dockcross-linux-ppc64le
-chmod +x ./dockcross-linux-ppc64le
+sudo apt install -y gcc-multilib-powerpc-linux-gnu g++-7-powerpc-linux-gnu
 echo "Installing dockcross image for x86 C++ cross-compiler"
 docker pull dockcross/linux-x86
 docker run --rm dockcross/linux-x86 > ./dockcross-linux-x86
