8d0a0607735e44d5e136af2c361fb409d33663e6
Thomas Barabosch <thomas.barabosch@gmail.com>
2019-02-08 16:10:31 +0100

    Cross compilers for Ubuntu 16.04 as Travis demands it.

--- test/artificial_samples/install_cross_compilers.sh
+++ test/artificial_samples/install_cross_compilers.sh
@@ -2,11 +2,7 @@
 echo "Installing cross compiler for ARM architecture."
 sudo apt install -y gcc-multilib-arm-linux-gnueabi g++-arm-linux-gnueabi
 echo "Installing cross compiler for MIPS architecture."
-sudo apt install -y gcc-multilib-mips-linux-gnu g++-7-mips-linux-gnu
+sudo apt install -y gcc-multilib-mips-linux-gnu g++-5-mips-linux-gnu
 echo "Installing cross compiler for PPC architecture."
-sudo apt install -y gcc-multilib-powerpc-linux-gnu g++-7-powerpc-linux-gnu
-echo "Installing dockcross image for x86 C++ cross-compiler"
-docker pull dockcross/linux-x86
-docker run --rm dockcross/linux-x86 > ./dockcross-linux-x86
-chmod +x ./dockcross-linux-x86
+sudo apt install -y gcc-multilib-powerpc-linux-gnu g++-5-powerpc-linux-gnu
 echo "Done."
