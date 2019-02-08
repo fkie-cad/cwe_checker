567658a4093f5d0592dc322a76f90fb89b539000
Thomas Barabosch <thomas.barabosch@gmail.com>
2019-02-08 19:13:48 +0100

    Added dockcross again for x86, fixed

--- test/artificial_samples/install_cross_compilers.sh
+++ test/artificial_samples/install_cross_compilers.sh
@@ -7,6 +7,6 @@ echo "Installing cross compiler for PPC architecture."
 sudo apt install -y gcc-multilib-powerpc-linux-gnu g++-5-powerpc-linux-gnu
 echo "Installing dependencies for x86 compilation"
 sudo docker pull dockcross/linux-x86
-sudo docker run -rm dockcross/linux-x86 > ./dockcross-linux-x86
+sudo docker run --rm dockcross/linux-x86 > ./dockcross-linux-x86
 chmod +x ./dockcross-linux-x86
 echo "Done."
