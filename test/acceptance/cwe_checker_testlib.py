35e3506eb7240713902d5c0c3abd774724de921b
Thomas Barabosch <thomas.barabosch@fkie.fraunhofer.de>
2019-02-21 14:18:04 +0100

    Fixed another Python file due to precommit

--- test/acceptance/cwe_checker_testlib.py
+++ test/acceptance/cwe_checker_testlib.py
@@ -1,8 +1,10 @@
 import subprocess
 
+
 def build_bap_cmd(filename, target, arch):
-        cmd = 'bap test/artificial_samples/build/cwe_%s_%s.out  --pass=callsites,cwe-checker --cwe-checker-partial=CWE%s --cwe-checker-config=src/config.json' % (filename, arch, target)
-        return cmd.split()
+    cmd = 'bap test/artificial_samples/build/cwe_%s_%s.out  --pass=callsites,cwe-checker --cwe-checker-partial=CWE%s --cwe-checker-config=src/config.json' % (filename, arch, target)
+    return cmd.split()
+
 
 def execute_and_check_occurence(filename, target, arch, string):
     occurence = 0
