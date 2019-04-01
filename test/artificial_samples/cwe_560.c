7a73640f0f6af7d7f4fcbc644237ef43baa91223
Thomas Barabosch <thomas.barabosch@gmail.com>
2019-04-01 18:41:08 +0200

    Initial version of CWE560 check

---
+++ test/artificial_samples/cwe_560.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+void umask_incorrect(){
+        umask(666);
+        int fd = open("some_random_file", O_CREAT|O_WRONLY, 0666);
+        close(fd);
+}
+
+void umask_correct(){
+        umask(022);
+        int fd = open("some_random_file", O_CREAT|O_WRONLY, 0666);
+        close(fd);
+}
+
+int main(){
+        umask_correct();
+        umask_incorrect();
+        return 0;
+}
