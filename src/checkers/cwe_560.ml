7cfc3e979ae84ce93a47b8924bb2d2e1b7ebbd4f
Thomas Barabosch <thomas.barabosch@gmail.com>
2019-04-01 18:41:08 +0200

    Initial version of CWE560 check

--- src/checkers/cwe_560.ml
+++ src/checkers/cwe_560.ml
@@ -1,23 +1,49 @@
-open Bap.Std
 open Core_kernel
+open Bap.Std
 
 let name = "CWE560"
 let version = "0.1"
 
-let check_umask_call program proj tid_map sub umask_call =
-  Log_utils.warn "CHECKING"
+let upper_bound_of_correct_umask_arg_value = 100
+let upper_bound_of_correct_chmod_arg_value = 1000
+
+ let collect_int_values = Exp.fold ~init:[] (object
+          inherit [word list] Exp.visitor
+          method! enter_int x addrs = x :: addrs
+        end)
+
+let check_umask_arg tid_map blk w =
+  try
+    let umask_arg = Word.to_int_exn w in
+    if umask_arg  > upper_bound_of_correct_umask_arg_value && umask_arg < upper_bound_of_correct_chmod_arg_value then
+      Log_utils.warn "[%s] {%s} (Use of umask() with chmod-style Argument) Function %s calls umask with argument %d"
+        name
+        version
+        (Address_translation.translate_tid_to_assembler_address_string (Term.tid blk) tid_map)
+        umask_arg
+  with _ -> Log_utils.error "Caught exception in module [CWE560]."
+
+let check_umask_call program proj tid_map blk =
+  Seq.iter (Term.enum def_t blk) ~f:(fun d ->
+    match Exp.eval @@ Def.rhs d with
+    | Imm w -> check_umask_arg tid_map blk w
+    | _ -> ();
+        (* TODO: iterate over all terms with Exp.iter to collect all imms *)
+   let rhs = Def.rhs d in
+   let int_values = collect_int_values rhs in
+   List.iter int_values ~f:(fun x -> check_umask_arg tid_map blk x)
+  )
+
+let blk_calls_umask sym_umask sub blk =
+  Term.enum jmp_t blk
+  |> Seq.exists ~f:(fun callsite -> Symbol_utils.calls_callsite_symbol callsite sym_umask)
 
 let check_subfunction program proj tid_map sym_umask sub =
   if Symbol_utils.sub_calls_symbol program sub "umask" then
     begin
-      Log_utils.warn "[%s] {%s} (Use of umask() with chmod-style Argument) Function %s calls umask"
-                                             name
-                                             version
-                                             (Sub.name sub);
-      Symbol_utils.get_direct_callsites_of_sub sub
-      |> Seq.iter ~f:(fun x -> if Symbol_utils.calls_callsite_symbol x sym_umask
-                       then check_umask_call program proj tid_map sub x
-                       else ())
+      Term.enum blk_t sub
+      |> Seq.filter ~f:(fun blk -> blk_calls_umask sym_umask sub blk)
+      |> Seq.iter ~f:(fun blk -> check_umask_call program proj tid_map blk)
     end
   else
     ()
@@ -26,7 +52,7 @@ let check_subfunctions program proj tid_map sym_umask =
   Seq.iter (Term.enum sub_t program) ~f:(fun sub -> check_subfunction program proj tid_map sym_umask sub)
 
 let check_cwe program proj tid_map _ _ =
-  let sym = Symbol_utils.get_symbol_of_string program "umask" in
-  match sym with
-  | None -> ()
-  | Some sym_umask -> check_subfunctions program proj tid_map sym_umask
+    let sym = Symbol_utils.get_symbol_of_string program "umask" in
+    match sym with
+    | None -> ()
+    | Some sym_umask -> check_subfunctions program proj tid_map sym_umask
