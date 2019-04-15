8abea1e03d2fd0d20377c57113ada4c98bd9f699
Thomas Barabosch <thomas.barabosch@fkie.fraunhofer.de>
2019-04-15 16:12:33 +0200

    Adds tests for cwe-415 and cwe-416. Arritifical examples for cwe-125 and cwe-787

---
+++ test/acceptance/test_cwe416.py
@@ -0,0 +1,35 @@
+import unittest
+import cwe_checker_testlib
+
+
+class TestCwe416(unittest.TestCase):
+
+    def setUp(self):
+        self.target = '416'
+        self.string = b'Use After Free'
+
+    def test_cwe416_01_arm(self):
+        expect_res = 2
+        res = cwe_checker_testlib.execute_emulation_and_check_occurence(self.target, self.target, 'arm', self.string)
+        assert res == expect_res
+
+    def test_cwe416_01_x86(self):
+        expect_res = 2
+        res = cwe_checker_testlib.execute_emulation_and_check_occurence(self.target, self.target, 'x86', self.string)
+        assert res == expect_res
+
+    def test_cwe416_01_x64(self):
+        expect_res = 1
+        res = cwe_checker_testlib.execute_emulation_and_check_occurence(self.target, self.target, 'x64', self.string)
+        assert res == expect_res
+
+    @unittest.skip("Depends on proper MIPS support in BAP")
+    def test_cwe416_01_mips(self):
+        expect_res = 1
+        res = cwe_checker_testlib.execute_emulation_and_check_occurence(self.target, self.target, 'mips', self.string)
+        assert res == expect_res
+
+    def test_cwe416_01_ppc(self):
+        expect_res = 1
+        res = cwe_checker_testlib.execute_emulation_and_check_occurence(self.target, self.target, 'ppc', self.string)
+        assert res == expect_res
--- test/artificial_samples/cwe_787.c
+++ test/artificial_samples/cwe_787.c
@@ -7,5 +7,5 @@ int main(int argc, char **argv) {
         id_sequence[0] = 123;
         id_sequence[1] = 234;
         id_sequence[2] = 345;
-        id_sequence[0] = id_sequence[3];
+        id_sequence[3] = 678;
 }
