25eabd367bac4b28b2033b2fc431834ae8d09fce
Enkelmann <nils-edvin.enkelmann@fkie.fraunhofer.de>
2019-06-18 14:38:57 +0200

    Adjusted to newer Yojson versions

--- src/utils/json_utils.mli
+++ src/utils/json_utils.mli
@@ -1,5 +1,5 @@
 (* This module implements functionality related to parsing the JSON configuration file. *)
 
-val get_symbol_lists_from_json : Yojson.Basic.json -> string -> string list list
-val get_symbols_from_json : Yojson.Basic.json -> string -> string list
-val get_parameter_list_from_json : Yojson.Basic.json -> string -> string list
+val get_symbol_lists_from_json : Yojson.Basic.t -> string -> string list list
+val get_symbols_from_json : Yojson.Basic.t -> string -> string list
+val get_parameter_list_from_json : Yojson.Basic.t -> string -> string list
