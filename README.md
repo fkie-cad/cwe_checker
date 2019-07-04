d0afce031a2af32bfc946963c4e100e491dc4dab
Thomas Barabosch <14945594+tbarabosch@users.noreply.github.com>
2019-07-04 15:32:59 +0200

    Improve ida pro support and fix issue #24 (#25)
    * This commit improves the cwe_checker_to_ida tool. First, it fixes
    issue #24. Second, it introduces some unit tests for
    cwe_checker_to_ida. Third, cwe_checker_to_ida parses newer cwe checks
    like cwe415 or cwe787. Forth, updated description of
    cwe_checker_to_ida in README.md.

--- README.md
+++ README.md
@@ -32,9 +32,12 @@ Its main focus are ELF binaries that are commonly found on Linux and Unix operat
 
 Please note that some of the above analyses only are partially implemented at the moment. Furthermore, false positives are to be expected due to shortcuts and the nature of static analysis as well as over-approximation.
 
-*cwe_checker* comes with a script called `cwe_checker_to_ida`, which parses the output of *cwe_checker* and generates a IDAPython script. This script annotates the found CWEs in IDA Pro, which helps during manual analysis of a binary. The following screenshot shows some results:
+*cwe_checker* comes with a script called `cwe_checker_to_ida`, which parses the output of *cwe_checker* and generates a IDAPython script. This script annotates the found CWEs in IDA Pro, which helps during manual analysis of a binary. The colors represent the  severeness of the found issues (yellow, orange, or red). The following screenshot shows some results:
 
-![](https://github.com/fkie-cad/cwe_checker/raw/master/doc/images/example_ida_anotation.png "IDA Pro anotation")
+<p align="center">
+    <img src="doc/images/example_ida_anotation.png" alt="IDA Pro anotation" width="50%" height="50%"/>
+</p>
+
 ## Why use cwe_checker? ##
 The following arguments should convince you to give *cwe_checker* a try:
 -  it is very easy to setup, just build the Docker container!
--- cwe_checker_to_ida/CweCheckerParser.py
+++ cwe_checker_to_ida/CweCheckerParser.py
@@ -1,16 +1,25 @@
 import logging
 
-colors = {"CWE190": "0xBBBBBB",
-          "CWE215": None,
-          "CWE243": None,
-          "CWE332": None,
-          "CWE367": "0xFF15AA",
-          "CWE426": "0xAA15FF",
-          "CWE457": "0x4286F4",
-          "CWE467": "0xFFD400",
-          "CWE476": "0x2AFF00",
-          "CWE676": "0x8833FF",
-          "CWE-782": "0xCC00BB",
+RED = 0x6666FF
+ORANGE = 0x6699FF
+YELLOW = 0xC0FFFF
+
+colors = {'CWE190': YELLOW,
+          'CWE215': None,
+          'CWE243': None,
+          'CWE248': YELLOW,
+          'CWE332': None,
+          'CWE367': ORANGE,
+          'CWE415': RED,
+          'CWE416': RED,
+          'CWE426': ORANGE,
+          'CWE457': YELLOW,
+          'CWE467': ORANGE,
+          'CWE476': ORANGE,
+          'CWE560': YELLOW,
+          'CWE676': RED,
+          'CWE782': ORANGE,
+          'CWE787': RED,
           }
 
 
@@ -43,7 +52,7 @@ class CweWarningParser(object):
         try:
             splitted_line = warning.split('WARN')
             cwe_warning = splitted_line[1].replace(
-                'u32', '').replace("64u", '').replace(':', '')
+                'u32', '').replace('64u', '').replace(':', '')
 
             cwe_name = self._remove_color(cwe_warning.split(')')[0]) + ')'
             cwe_name = cwe_name.split('{')[0].strip() + ' ' + cwe_name.split('}')[1].strip()
@@ -55,7 +64,7 @@ class CweWarningParser(object):
 
             return CweWarning(cwe_name, plugin_version, cwe_message)
         except Exception as e:
-            logging.error('[CweWarningParser] Error while parsing CWE warning: %s.' % str(e))
+            logging.error('[CweWarningParser] Error while parsing CWE warning: %s.', str(e))
             return None
 
 
@@ -63,74 +72,95 @@ class Parser(object):
 
     def __init__(self, result_path):
         self._result_path = result_path
-        self._parsers = {"CWE190": self._parse_cwe190,
-                         "CWE215": self._not_highlighted,
-                         "CWE243": self._not_highlighted,
-                         "CWE332": self._not_highlighted,
-                         "CWE367": self._parse_at,
-                         "CWE426": self._parse_at,
-                         "CWE457": self._parse_cwe457,
-                         "CWE467": self._parse_cwe467,
-                         "CWE476": self._parse_cwe476,
-                         "CWE676": self._parse_cwe676,
-                         "CWE782": self._parse_cwe782,
+        self._parsers = {'CWE125': self.parse_path,
+                         'CWE190': self.parse_cwe190,
+                         'CWE215': self.not_highlighted,
+                         'CWE243': self.not_highlighted,
+                         'CWE248': self.parse_at,
+                         'CWE332': self.not_highlighted,
+                         'CWE367': self.parse_at,
+                         'CWE415': self.parse_path,
+                         'CWE416': self.parse_path,
+                         'CWE426': self.parse_at,
+                         'CWE457': self.parse_cwe457,
+                         'CWE467': self.parse_cwe467,
+                         'CWE476': self.parse_cwe476,
+                         'CWE560': self.parse_cwe560,
+                         'CWE676': self.parse_cwe676,
+                         'CWE782': self.parse_cwe782,
+                         'CWE787': self.parse_path,
                          }
 
     def _read_in_config(self):
         lines = None
-        with open(self._result_path, "r") as f:
+        with open(self._result_path, 'r') as f:
             lines = f.readlines()
         if not lines:
-            print("[Parser] Could not read in file %s" % self._result_path)
+            print('[Parser] Could not read in file %s' % self._result_path)
             raise Exception()
         return lines
 
     @staticmethod
-    def _not_highlighted(warning):
+    def not_highlighted(warning):
         warning.highlight = False
         return warning
 
     @staticmethod
-    def _parse_at(warning):
-        warning.address = warning.warning.split("at ")[-1].split()[0].strip()
+    def parse_at(warning):
+        warning.address = warning.warning.split('at ')[-1].split()[0].strip()
         return warning
 
     @staticmethod
-    def _parse_cwe190(warning):
-        if "multiplication" in warning.warning:
-            warning.address = warning.warning.split("multiplication ")[1].split()[0]
+    def parse_path(warning):
+        warning.address = warning.warning.split('->')[-1].strip()
+        return warning
+
+    @staticmethod
+    def parse_cwe190(warning):
+        if 'multiplication' in warning.warning:
+            warning.address = warning.warning.split('multiplication ')[1].split()[0]
         else:
-            warning.address = warning.warning.split("addition ")[1].split()[0]
+            warning.address = warning.warning.split('addition ')[1].split()[0]
         return warning
 
     @staticmethod
-    def _parse_cwe457(warning):
-        warning.address = warning.warning.split("at ")[-1].split(":")[0].strip()
+    def parse_cwe248(warning):
+        warning.address = warning.warning.split('at ')[-1].split(':')[0].strip()
         return warning
 
     @staticmethod
-    def _parse_cwe467(warning):
-        warning.address = warning.warning.split("at ")[1].split()[0]
+    def parse_cwe457(warning):
+        warning.address = warning.warning.split('at ')[-1].split(':')[0].strip()
         return warning
 
     @staticmethod
-    def _parse_cwe476(warning):
-        warning.address = warning.warning.split("at ")[1].split()[0]
+    def parse_cwe467(warning):
+        warning.address = warning.warning.split('at ')[1].split()[0]
         return warning
 
     @staticmethod
-    def _parse_cwe676(warning):
-        warning.address = warning.warning.split("(")[1].split(")")[0].split(":")[0]
+    def parse_cwe476(warning):
+        warning.address = warning.warning.split('at ')[1].split()[0]
         return warning
 
     @staticmethod
-    def _parse_cwe782(warning):
-        warning.address = warning.warning.spit("(")[1].split(")")[0].strip()
+    def parse_cwe560(warning):
+        warning.address = warning.warning.split('Function ')[1].split()[0]
+        return warning
+
+    @staticmethod
+    def parse_cwe676(warning):
+        warning.address = warning.warning.split('(')[-1].split(')')[0].split(':')[0]
+        return warning
+
+    @staticmethod
+    def parse_cwe782(warning):
+        warning.address = warning.warning.split('(')[1].split(')')[0].strip()
         return warning
 
     @staticmethod
     def _extract_cwe_number(name):
-        tmp = name.split("]")[0]
+        tmp = name.split(']')[0]
         return tmp[1:]
 
     def parse(self):
@@ -140,18 +170,14 @@ class Parser(object):
         for line in lines:
             line = line.strip()
             if 'WARN' in line:
-                try:
-                    warning = cwe_parser.parse(line)
-                    cwe_number = self._extract_cwe_number(warning.name)
-                    warning.cwe_number = cwe_number
-                    if cwe_number in self._parsers:
-                        warning = self._parsers[cwe_number](warning)
-                        warning.color = colors[warning.cwe_number]
-                        if warning.address != "UNKNOWN":
-                            result.append(warning)
-                    else:
-                        print("Warning: %s not supported." % cwe_number)
-                except Exception as e:
-                    print('Error while parsing: %s' % str(e))
-                    print(line)
+                warning = cwe_parser.parse(line)
+                cwe_number = self._extract_cwe_number(warning.name)
+                warning.cwe_number = cwe_number
+                if cwe_number in self._parsers:
+                    warning = self._parsers[cwe_number](warning)
+                    warning.color = colors[warning.cwe_number]
+                    if warning.address != 'UNKNOWN':
+                        result.append(warning)
+                else:
+                    print('Warning: %s not supported.' % cwe_number)
         return result
