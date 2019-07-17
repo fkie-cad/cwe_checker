ce30db364e356b46f5f4f65e227c5bf884d8defc
Thomas Barabosch <thomas.barabosch@fkie.fraunhofer.de>
2019-07-17 15:25:47 +0200

    Added feature to compiler test cases with more than one compiler. Added clang as first examples. Fixed test cases to work with gcc

--- test/acceptance/test_cwe190.py
+++ test/acceptance/test_cwe190.py
@@ -8,28 +8,28 @@ class TestCwe190(unittest.TestCase):
         self.target = '190'
         self.string = b'Integer Overflow or Wraparound'
 
-    def test_cwe190_01_arm(self):
+    def test_cwe190_01_arm_gcc(self):
         expect_res = 3
-        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'arm', self.string)
+        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'arm', 'gcc', self.string)
         self.assertEqual(res, expect_res)
 
-    def test_cwe190_01_x86(self):
+    def test_cwe190_01_x86_gcc(self):
         expect_res = 3
-        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'x86', self.string)
+        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'x86', 'gcc', self.string)
         self.assertEqual(res, expect_res)
 
-    def test_cwe190_01_x64(self):
+    def test_cwe190_01_x64_gcc(self):
         expect_res = 3
-        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'x64', self.string)
+        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'x64', 'gcc', self.string)
         self.assertEqual(res, expect_res)
 
     @unittest.skip("Depends on proper MIPS support in BAP")
-    def test_cwe190_01_mips(self):
+    def test_cwe190_01_mips_gcc(self):
         expect_res = 3
-        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'mips', self.string)
+        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'mips', 'gcc', self.string)
         self.assertEqual(res, expect_res)
 
-    def test_cwe190_01_ppc(self):
+    def test_cwe190_01_ppc_gcc(self):
         expect_res = 3
-        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'ppc', self.string)
+        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'ppc', 'gcc', self.string)
         self.assertEqual(res, expect_res)
--- test/acceptance/test_cwe215.py
+++ test/acceptance/test_cwe215.py
@@ -9,27 +9,27 @@ class TestCwe215(unittest.TestCase):
         self.filename = '476'
         self.string = b'Information Exposure Through Debug Information'
 
-    def test_cwe215_01_arm(self):
+    def test_cwe215_01_arm_gcc(self):
         expect_res = 1
-        res = cwe_checker_testlib.execute_and_check_occurence(self.filename, self.target, 'arm', self.string)
+        res = cwe_checker_testlib.execute_and_check_occurence(self.filename, self.target, 'arm', 'gcc', self.string)
         self.assertEqual(res, expect_res)
 
-    def test_cwe215_01_x86(self):
+    def test_cwe215_01_x86_gcc(self):
         expect_res = 1
-        res = cwe_checker_testlib.execute_and_check_occurence(self.filename, self.target, 'x86', self.string)
+        res = cwe_checker_testlib.execute_and_check_occurence(self.filename, self.target, 'x86', 'gcc', self.string)
         self.assertEqual(res, expect_res)
 
-    def test_cwe215_01_x64(self):
+    def test_cwe215_01_x64_gcc(self):
         expect_res = 1
-        res = cwe_checker_testlib.execute_and_check_occurence(self.filename, self.target, 'x64', self.string)
+        res = cwe_checker_testlib.execute_and_check_occurence(self.filename, self.target, 'x64', 'gcc', self.string)
         self.assertEqual(res, expect_res)
 
-    def test_cwe215_01_ppc(self):
+    def test_cwe215_01_ppc_gcc(self):
         expect_res = 1
-        res = cwe_checker_testlib.execute_and_check_occurence(self.filename, self.target, 'ppc', self.string)
+        res = cwe_checker_testlib.execute_and_check_occurence(self.filename, self.target, 'ppc', 'gcc', self.string)
         self.assertEqual(res, expect_res)
 
-    def test_cwe215_01_mips(self):
+    def test_cwe215_01_mips_gcc(self):
         expect_res = 1
-        res = cwe_checker_testlib.execute_and_check_occurence(self.filename, self.target, 'mips', self.string)
+        res = cwe_checker_testlib.execute_and_check_occurence(self.filename, self.target, 'mips', 'gcc', self.string)
         self.assertEqual(res, expect_res)
