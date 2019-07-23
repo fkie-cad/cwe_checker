cebc4f85e2adc72da396be2d4531105c8f6a2078
Thomas Barabosch <thomas.barabosch@gmail.com>
2019-07-23 17:04:28 +0200

    Fixed acceptance test.

--- test/acceptance/test_json.py
+++ test/acceptance/test_json.py
@@ -1,3 +1,6 @@
+import os
+import subprocess
+import json
 import unittest
 
 class TestJson(unittest.TestCase):
@@ -5,11 +8,11 @@ class TestJson(unittest.TestCase):
     def setUp(self):
         if 'travis' in os.environ['USER']:
             abs_path = os.path.abspath('test/artificial_samples/build/cwe_190_x64.out')
-            self.cmd = 'docker run --rm -v %s:/tmp/input cwe-checker:latest bap /tmp/input --pass=cwe-checker --cwe-checker-partial=CWE19  --cwe-checker-config=/home/bap/cwe_checker/src/config.json' % abs_path
+            self.cmd = 'docker run --rm -v %s:/tmp/input cwe-checker:latest bap /tmp/input --pass=cwe-checker  --cwe-checker-config=/home/bap/cwe_checker/src/config.json --cwe-checker-json' % abs_path
         else:
-            self.cmd = 'bap test/artificial_samples/build/cwe_190_x64.out --pass=cwe-checker --cwe-checker-partial=CWE190 --cwe-checker-config=src/config.json'
+            self.cmd = 'bap test/artificial_samples/build/cwe_190_x64.out --pass=cwe-checker --cwe-checker-config=src/config.json --cwe-checker-json'
 
     def test_can_output_json(self):
         output = subprocess.check_output(self.cmd.split())
         j = json.loads(output)
-        self.assertNotEqual(j, None)
+        self.assertTrue('warnings' in j)
