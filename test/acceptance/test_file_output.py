6241889303920c658b248b7f3a981f1faf15e880
Thomas Barabosch <thomas.barabosch@gmail.com>
2019-07-24 12:08:18 +0200

    adjusted acceptance test for travis

--- test/acceptance/test_file_output.py
+++ test/acceptance/test_file_output.py
@@ -7,13 +7,16 @@ class TestFileOutput(unittest.TestCase):
 
     def setUp(self):
         if 'travis' in os.environ['USER']:
+            self.res_file = 'res.json'
             abs_path = os.path.abspath('test/artificial_samples/build/cwe_190_x64.out')
-            self.cmd = 'docker run --rm -v %s:/tmp/input cwe-checker:latest bap /tmp/input --pass=cwe-checker  --cwe-checker-config=/home/bap/cwe_checker/src/config.json --cwe-checker-json --cwe-checker-out=/tmp/res.json' % abs_path
+            self.cmd = 'docker run --rm -v %s:/tmp/input cwe-checker:latest bap /tmp/input --pass=cwe-checker  --cwe-checker-config=/home/bap/cwe_checker/src/config.json --cwe-checker-json --cwe-checker-out=%s' % (abs_path, self.res_file)
         else:
-            self.cmd = 'bap test/artificial_samples/build/cwe_190_x64.out --pass=cwe-checker --cwe-checker-config=src/config.json --cwe-checker-json --cwe-checker-out=/tmp/res.json'
+            self.res_file = '/tmp/res.json'
+            self.cmd = 'bap test/artificial_samples/build/cwe_190_x64.out --pass=cwe-checker --cwe-checker-config=src/config.json --cwe-checker-json --cwe-checker-out=%s' % self.res_file
 
     def test_can_output_file(self):
         subprocess.check_output(self.cmd.split())
-        with open('/tmp/res.json') as f:
+        with open(self.res_file) as f:
             j = json.load(f)
-            self.assertTrue('warnings' in j)
+        os.remove(self.res_file)
+        self.assertTrue('warnings' in j)
