ac752d5895802d2363329ee54d065e4963b0b152
Thomas Barabosch <14945594+tbarabosch@users.noreply.github.com>
2019-07-31 10:08:23 +0200

    Fix cwe_checker_to_ida, which was broken due to refactoring of logging (#35)
    This fixes issues #34

--- cwe_checker_to_ida/Generator.py
+++ cwe_checker_to_ida/Generator.py
@@ -6,10 +6,11 @@ class IdaGenerator(object):
     def generate(self):
         script = "import sark\nimport idaapi\n"
         for res in self._results:
-            if res.highlight:
-                script += "sark.Line(%s).color = %s\n" % (res.address, res.color)
-                script += "sark.Line(%s).comments.regular = '%s'\n" % (res.address, res.name)
-                script += "print('[ %s ] %s')\n" % (res.address, res.name)
+            if res.highlight and res.address:
+                first_address = res.address[0]
+                script += "sark.Line(%s).color = %s\n" % (first_address, res.color)
+                script += "sark.Line(%s).comments.regular = '%s'\n" % (first_address, res.description)
+                script += "print('[ %s ] %s')\n" % (first_address, res.description)
             else:
-                script += "print('[ GENERAL ] %s')\n" % res.name
+                script += "print('[ GENERAL ] %s')\n" % res.description
         return script
--- cwe_checker_to_ida/cwe_checker_to_ida.py
+++ cwe_checker_to_ida/cwe_checker_to_ida.py
@@ -1,3 +1,5 @@
+import os
+import json
 import argparse
 
 from CweCheckerParser import Parser
@@ -9,7 +11,7 @@ def parse_args():
         description='Generates an anotation script for IDA Pro based on CweChecker results.')
     parser.add_argument(
         '-i', '--cwe_checker_result', type=str, required=True,
-        help='The path to the output of CweChecker.')
+        help='The path to the JSON output of CweChecker.')
     parser.add_argument(
         '-o', '--anotation_script_output', type=str, required=True,
         help='The output path of the anotation script.')
@@ -18,15 +20,35 @@ def parse_args():
 
 
 def save_generated_script(outpath, generated_script):
-    with open(outpath, "w") as f:
-        f.write(generated_script)
+    with open(outpath, "w") as fhandle:
+        fhandle.write(generated_script)
 
+def is_valid_json_file(fpath):
+    try:
+        with open(fpath) as fhandle:
+            json.load(fhandle)
+            return True
+    except json.JSONDecodeError:
+        pass
+
+    return False
 
 def main():
     args = parse_args()
+
+    if not os.path.isfile(args.cwe_checker_result):
+        print('Input file does not exist.')
+        return 1
+
+    if not is_valid_json_file(args.cwe_checker_result):
+        print('Input file must be formatted as cwe_checker\'s JSON output.')
+        return 1
+
     results = Parser(args.cwe_checker_result).parse()
     generated_script = IdaGenerator(results).generate()
     save_generated_script(args.anotation_script_output, generated_script)
+    print('Done. Now execute generated script %s with IDAPython (alt+F9).'
+          % args.anotation_script_output)
 
 
 if __name__ == '__main__':
