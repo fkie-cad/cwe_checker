5e3d71d0352f3278ade856778312d39d1aa94ed4
Thomas Barabosch <thomas.barabosch@fkie.fraunhofer.de>
2019-07-31 08:13:11 +0200

    This fixes issues #34

--- cwe_checker_to_ida/cwe_checker_to_ida.py
+++ cwe_checker_to_ida/cwe_checker_to_ida.py
@@ -9,7 +9,7 @@ def parse_args():
         description='Generates an anotation script for IDA Pro based on CweChecker results.')
     parser.add_argument(
         '-i', '--cwe_checker_result', type=str, required=True,
-        help='The path to the output of CweChecker.')
+        help='The path to the JSON output of CweChecker.')
     parser.add_argument(
         '-o', '--anotation_script_output', type=str, required=True,
         help='The output path of the anotation script.')
@@ -24,9 +24,15 @@ def save_generated_script(outpath, generated_script):
 
 def main():
     args = parse_args()
+
+    if not args.cwe_checker_result.endswith('.json'):
+        print('Input file must be formatted as cwe_checker\'s JSON output.')
+        return 1
+
     results = Parser(args.cwe_checker_result).parse()
     generated_script = IdaGenerator(results).generate()
     save_generated_script(args.anotation_script_output, generated_script)
+    print('Done. Now execute generated script %s with IDAPython (alt+F9).' % args.anotation_script_output)
 
 
 if __name__ == '__main__':
