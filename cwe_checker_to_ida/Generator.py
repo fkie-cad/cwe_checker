261aa992660ffe00f8856afa9a5d4da538e03cb3
Thomas Barabosch <thomas.barabosch@fkie.fraunhofer.de>
2019-09-10 11:46:55 +0200

    Added support for check_path to cwe_checker_to_ida scripts

--- cwe_checker_to_ida/Generator.py
+++ cwe_checker_to_ida/Generator.py
@@ -1,3 +1,5 @@
+from CweCheckerParser import CweWarning
+
 class IdaGenerator(object):
 
     def __init__(self, results):
@@ -6,11 +8,17 @@ class IdaGenerator(object):
     def generate(self):
         script = "import sark\nimport idaapi\n"
         for res in self._results:
-            if res.highlight and res.address:
-                first_address = res.address[0]
-                script += "sark.Line(%s).color = %s\n" % (first_address, res.color)
-                script += "sark.Line(%s).comments.regular = '%s'\n" % (first_address, res.description)
-                script += "print('[ %s ] %s')\n" % (first_address, res.description)
+            if type(res) == CweWarning:
+                if res.highlight and res.address:
+                    first_address = res.address[0]
+                    script += "sark.Line(%s).color = %s\n" % (first_address, res.color)
+                    script += "sark.Line(%s).comments.regular = '%s'\n" % (first_address, res.description)
+                    script += "print('[ %s ] %s')\n" % (first_address, res.description)
+                else:
+                    script += "print('[ GENERAL ] %s')\n" % res.description
             else:
-                script += "print('[ GENERAL ] %s')\n" % res.description
+                script += "print('[CheckPath] %s ( %s ) -> %s via %s')\n" % (res.source,
+                                                                           res.source_addr,
+                                                                           res.destination,
+                                                                           res.path_str)
         return script
