d86f3e0c7b5f8183f09ddbcb313c4a0176e65a2d
mellowCS <cs.germany@protonmail.com>
2019-12-20 10:37:52 +0100

    cpp test files were given the 'gcc' abbreviation even though the .out files were given the name 'g++'. This might have broken the test test_cwe248.py.

--- test/acceptance/test_cwe248.py
+++ test/acceptance/test_cwe248.py
@@ -10,33 +10,33 @@ class TestCwe248(unittest.TestCase):
 
     def test_cwe248_01_arm_gcc(self):
         expect_res = 2
-        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'arm', 'gcc', self.string)
+        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'arm', 'g++', self.string)
         self.assertEqual(res, expect_res)
 
     @unittest.skip("Fix CPP compilation issue for x86")
     def test_cwe248_01_x86_gcc(self):
         expect_res = 2
-        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'x86', 'gcc', self.string)
+        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'x86', 'g++', self.string)
         self.assertEqual(res, expect_res)
 
     def test_cwe248_01_x64_gcc(self):
         expect_res = 2
-        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'x64', 'gcc', self.string)
+        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'x64', 'g++', self.string)
         self.assertEqual(res, expect_res)
 
     def test_cwe248_01_x64_clang(self):
         expect_res = 2
-        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'x64', 'clang', self.string)
+        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'x64', 'clang++', self.string)
         self.assertEqual(res, expect_res)
 
     @unittest.skip("Depends on proper MIPS support in BAP")
     def test_cwe248_01_mips_gcc(self):
         expect_res = 2
-        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'mips', 'gcc', self.string)
+        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'mips', 'g++', self.string)
         self.assertEqual(res, expect_res)
 
     @unittest.skip("FIXME")
     def test_cwe248_01_ppc_gcc(self):
         expect_res = 2
-        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'ppc', 'gcc', self.string)
+        res = cwe_checker_testlib.execute_and_check_occurence(self.target, self.target, 'ppc', 'g++', self.string)
         self.assertEqual(res, expect_res)
