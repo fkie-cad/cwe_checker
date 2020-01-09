d386b76388a98aafc176b6a433a120eb74758af0
Melvin Klimke <43435036+mellowCS@users.noreply.github.com>
2020-01-09 13:13:56 +0100

    Added skip for x64 PE test

--- test/acceptance/test_cwe457.py
+++ test/acceptance/test_cwe457.py
@@ -162,6 +162,7 @@ class TestCwe457(unittest.TestCase):
             self.target, self.target, 'x86', 'mingw32-gcc', self.string)
         self.assertEqual(res, expect_res)
 
+    @unittest.skip('FIXME!')
     def test_cwe457_01_x64_mingw_gcc(self):
         expect_res = 3
         res = cwe_checker_testlib.execute_and_check_occurence(
