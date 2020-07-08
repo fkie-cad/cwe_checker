30879ad96b7a540b063995b8fbc73e376d775062
Enkelmann <nils-edvin.enkelmann@fkie.fraunhofer.de>
2020-07-08 11:13:38 +0200

    Added -no-logging to some acceptance tests

--- test/acceptance/test_json.py
+++ test/acceptance/test_json.py
@@ -8,9 +8,9 @@ class TestJson(unittest.TestCase):
     def setUp(self):
         if 'travis' in os.environ['USER']:
             abs_path = os.path.abspath('test/artificial_samples/build/cwe_190_x64_gcc.out')
-            self.cmd = 'docker run --rm -v %s:/tmp/input cwe-checker:latest bap /tmp/input --pass=cwe-checker  --cwe-checker-config=/home/bap/cwe_checker/src/config.json --cwe-checker-json' % abs_path
+            self.cmd = 'docker run --rm -v %s:/tmp/input cwe-checker:latest cwe_checker /tmp/input -config=/home/bap/cwe_checker/src/config.json -json -no-logging' % abs_path
         else:
-            self.cmd = 'bap test/artificial_samples/build/cwe_190_x64_gcc.out --pass=cwe-checker --cwe-checker-config=src/config.json --cwe-checker-json'
+            self.cmd = 'cwe_checker test/artificial_samples/build/cwe_190_x64_gcc.out -config=src/config.json -json -no-logging'
 
     def test_can_output_json(self):
         output = subprocess.check_output(self.cmd.split())
