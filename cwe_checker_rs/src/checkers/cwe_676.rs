c950a9096ea228ef1ace42ace8ad29d292f3bef8
mellowCS <cs.germany@protonmail.com>
2020-10-23 12:19:26 +0200

    changed docstring

--- cwe_checker_rs/src/checkers/cwe_676.rs
+++ cwe_checker_rs/src/checkers/cwe_676.rs
@@ -4,19 +4,19 @@ This module implements a check for CWE-676: Use of Potentially Dangerous Functio
     Potentially dangerous functions like memcpy can lead to security issues like buffer
     overflows.
 
-    See {: https://cwe.mitre.org/data/definitions/676.html} for a detailed description.
+    See https://cwe.mitre.org/data/definitions/676.html for a detailed description.
 
-    {1 How the check works}
+    1 How the check works
 
     Calls to dangerous functions are flagged. The list of functions that are considered
     dangerous can be configured in config.json. The default list is taken from
-    {: https://github.com/01org/safestringlib/wiki/SDL-List-of-Banned-Functions}.
+    https://github.com/01org/safestringlib/wiki/SDL-List-of-Banned-Functions.
 
-    {1 False Positives}
+    1 False Positives
 
     None known
 
-    {1 False Negatives}
+    1 False Negatives
 
     None known
 */
