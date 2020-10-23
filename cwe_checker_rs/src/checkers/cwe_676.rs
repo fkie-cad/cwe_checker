30afbd01f36b1fa2a8ac9169badcc314c6196a61
mellowCS <cs.germany@protonmail.com>
2020-10-23 11:12:54 +0200

    Added docstring for CWE 676

--- cwe_checker_rs/src/checkers/cwe_676.rs
+++ cwe_checker_rs/src/checkers/cwe_676.rs
@@ -1,4 +1,25 @@
-//! The CWE 676 checks for potentially dangerous function calls, such as strcpy, memcpy, scanf etc.
+/**
+This module implements a check for CWE-676: Use of Potentially Dangerous Function.
+
+    Potentially dangerous functions like memcpy can lead to security issues like buffer
+    overflows.
+
+    See {: https://cwe.mitre.org/data/definitions/676.html} for a detailed description.
+
+    {1 How the check works}
+
+    Calls to dangerous functions are flagged. The list of functions that are considered
+    dangerous can be configured in config.json. The default list is taken from
+    {: https://github.com/01org/safestringlib/wiki/SDL-List-of-Banned-Functions}.
+
+    {1 False Positives}
+
+    None known
+
+    {1 False Negatives}
+
+    None known
+*/
 use std::collections::HashMap;
 
 use crate::{
