e7e0aaec53ff02382e62f1d5f1aad65d98ef3fdc
Enkelmann <nils-edvin.enkelmann@fkie.fraunhofer.de>
2020-10-28 10:15:24 +0100

    Only use a single thread for acceptance tests

--- .travis_run_tests.sh
+++ .travis_run_tests.sh
@@ -3,4 +3,4 @@ docker run --rm -t cwe-checker make codestyle-check \
 && docker run --rm -t cwe-checker cargo test \
 && docker run --rm -t cwe-checker dune runtest \
 && pytest \
-&& docker run --rm -t cwe-checker-ghidra cargo test --no-fail-fast -p acceptance_tests_ghidra -- --show-output --ignored
+&& docker run --rm -t cwe-checker-ghidra cargo test --no-fail-fast -p acceptance_tests_ghidra -j 1 -- --show-output --ignored
