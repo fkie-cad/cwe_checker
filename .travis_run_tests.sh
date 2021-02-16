14c5f181c1c24690607c8cbe9a851e09292cbdce
Enkelmann <nils-edvin.enkelmann@fkie.fraunhofer.de>
2021-02-16 09:25:48 +0100

    Remove unused Travis CI files

--- .travis_run_tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker run --rm -t cwe-checker make codestyle-check \
-&& docker run --rm -t cwe-checker cargo test \
-&& docker run --rm -t cwe-checker dune runtest \
-&& pytest \
-&& docker run --rm -t cwe-checker-ghidra cargo test --no-fail-fast -p acceptance_tests_ghidra -- --show-output --ignored --test-threads 1
