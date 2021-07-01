e09783a465ef1fd817af45bf9eb9d72fdbc99182
mellowCS <cs.germany@protonmail.com>
2021-07-01 13:40:28 +0200

    merge origin master

--- src/cwe_checker_lib/src/pcode/term.rs
+++ src/cwe_checker_lib/src/pcode/term.rs
@@ -782,7 +782,6 @@ impl Project {
                 }
             })
             .collect();
-<<<<<<< HEAD
             IrProject {
                 program,
                 cpu_architecture: self.cpu_architecture,
@@ -796,21 +795,6 @@ impl Project {
                 register_list,
                 datatype_properties: self.datatype_properties.clone(),
             }
-=======
-        IrProject {
-            program,
-            cpu_architecture: self.cpu_architecture,
-            stack_pointer_register: self.stack_pointer_register.into(),
-            calling_conventions: self
-                .register_calling_convention
-                .clone()
-                .into_iter()
-                .map(|cconv| cconv.into())
-                .collect(),
-            register_list,
-            datatype_properties: self.datatype_properties,
-        }
->>>>>>> conflict fixes
     }
 }
 
