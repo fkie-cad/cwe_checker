824f49628f3e3fc773499098fd96b0d86d9e0b66
pr4gasm <77354315+sh4m2hwz@users.noreply.github.com>
2022-01-12 14:51:48 +0300

    fix cwe_checker_to_ida script (#279)

--- cwe_checker_to_ida/CweCheckerParser.py
+++ cwe_checker_to_ida/CweCheckerParser.py
@@ -22,69 +22,37 @@ colors = {'CWE190': YELLOW,
           'CWE787': RED,
           }
 
-class CheckPath(object):
+class Cwe:
 
-    def __init__(self, source, source_addr, destination, destination_addr, path_str):
-        self.source = source
-        self.source_addr = self.__fix_address(source_addr)
-        self.destination = self.__fix_address(destination)
-        self.destination_addr = self.__fix_address(destination_addr)
-        self.path_str = self.__fix_address(path_str)
-        self.color = None
-        self.highlight = False
+    def __init__(self, name, address, description):
+        self.address = '0x'+address
+        self.comment = description
+        self.color = self.__get_color(name)
 
-    @staticmethod
-    def __fix_address(address):
-        return address.replace(':32u', '').replace(':64u', '')
+    def __get_color(self,name):
+        return colors[name]
 
-class CweWarning(object):
+class Parser:
 
-    def __init__(self, name, plugin_version, description, addresses):
-        self.name = name
-        self.plugin_version = plugin_version
-        self.description = self.__fix_address(description)
-        self.color = None
-        self.address = [self.__fix_address(address) for address in addresses]
-        self.highlight = True
-
-    @staticmethod
-    def __fix_address(address):
-        return address.replace(':32u', '').replace(':64u', '')
-
-class Parser(object):
-
-    def __init__(self, result_path):
+    def __init__(self,result_path):
         self._result_path = result_path
-
-    @staticmethod
-    def _parse_cwe_warnings(j):
+    
+    def __parse_cwe(self,j):
         result = []
-
-        if 'warnings' in j:
-            for w in j['warnings']:
-                cwe_warning = CweWarning(w['name'], w['version'], w['description'], w['addresses'])
-                if cwe_warning.name in colors:
-                    cwe_warning.color = colors[cwe_warning.name]
-                else:
-                    cwe_warning.highlight = False
-                result.append(cwe_warning)
-
-        return result
-
-    @staticmethod
-    def _parse_check_path(j):
-        result = []
-
-        if 'check_path' in j:
-            for p in j['check_path']:
-                check_path = CheckPath(p['source'], p['source_addr'], p['destination'], p['destination_addr'], p['path_str'])
-                result.append(check_path)
+        for p in j:
+            addresses = p['addresses']
+            for address in addresses:
+                element = Cwe(
+                        address=address,
+                        name=p['name'],
+                        description=p['description'],
+                )
+                result.append(element)
 
         return result
 
     def parse(self):
         with open(self._result_path) as fhandle:
             j = json.load(fhandle)
-            warnings = self._parse_cwe_warnings(j)
-            check_path = self._parse_check_path(j)
-            return warnings + check_path
+            cwe_out = self.__parse_cwe(j)
+            return cwe_out
