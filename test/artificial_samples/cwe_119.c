1395f9969b5240720185c5a4d11fa446e94ff54a
Enkelmann <nils-edvin.enkelmann@fkie.fraunhofer.de>
2022-02-15 10:24:16 +0100

    include intraprocedural testcases in cwe_119.c

--- test/artificial_samples/cwe_119.c
+++ test/artificial_samples/cwe_119.c
@@ -17,10 +17,22 @@ void print_array_sum(int* array) {
 
 int main() {
     int* array = calloc(5, sizeof(int));
+    // intraprocedural buffer overflow
+    for(int i = 0; i<= 10; i++) {
+        array[i] = i*i; // Out-of-bounds write for arrays that are too small.
+    }
+    // interprocedural buffer overflow
     set_array_elements(array);
     free(array);
 
     array = malloc(5 * sizeof(int));
+    // intraprocedural buffer overflow
+    int sum = 0;
+    for(int i = 0; i<= 10; i++) {
+        sum += array[i]; // Out-of-bounds read for arrays that are too small.
+    }
+    printf("%d\n", sum);
+    // interprocedural buffer overflow
     print_array_sum(array);
 
     puts((void*) array - 1); // Parameter is an out-of-bounds pointer.
