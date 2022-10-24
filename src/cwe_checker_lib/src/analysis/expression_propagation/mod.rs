e70d334773ba14d231bd4a05e254d4718f92b77d
vandenBosch <mauritz.van.den.bosch@fkie.fraunhofer.de>
2022-10-24 12:18:54 +0200

    implemented review notes

--- src/cwe_checker_lib/src/analysis/expression_propagation/mod.rs
+++ src/cwe_checker_lib/src/analysis/expression_propagation/mod.rs
@@ -17,18 +17,12 @@ use std::collections::HashMap;
 /// This expressions can be used for expression propagation among basic blocks.
 pub struct Context<'a> {
     graph: &'a Graph<'a>,
-
-    /// Variable-expression lookup table primitive.
-    insertable_expressions: HashMap<Variable, Expression>,
 }
 
 impl<'a> Context<'a> {
     /// Create a new context object for the given project and control flow graph.
     pub fn new(_project: &'a Project, graph: &'a Graph) -> Context<'a> {
-        Context {
-            graph,
-            insertable_expressions: HashMap::new(),
-        }
+        Context { graph }
     }
 }
 
@@ -67,9 +61,8 @@ impl<'a> crate::analysis::forward_interprocedural_fixpoint::Context<'a> for Cont
                 insertable_expressions.insert(var.clone(), extended_expression.clone());
 
                 // Expressions dependent on the assigned variable are no longer insertable.
-                // Except, if the assigned variable depends on itself.
-                insertable_expressions.retain(|input_var, input_expr| {
-                    !(input_var != var && input_expr.input_vars().into_iter().any(|x| x == var))
+                insertable_expressions.retain(|_input_var, input_expr| {
+                    input_expr.input_vars().into_iter().any(|x| x == var)
                 });
 
                 Some(insertable_expressions)
@@ -153,13 +146,12 @@ fn compute_expression_propagation<'a>(
             // A start in the CFG has no incoming edges in the CFG and
             // are mostly due to cases where the control flow graph is incomplete.
             // We assume that no expressions are insertable at such starting nodes.
+            // Additionally, we initialize every function's entrypoint.
             if graph
                 .neighbors_directed(node, petgraph::Incoming)
                 .next()
                 .is_none()
-                || graph
-                    .edges(node)
-                    .any(|edge| matches!(edge.weight(), super::graph::Edge::Call(_)))
+                || graph[node].get_sub().term.blocks.first() == Some(graph[node].get_block())
             {
                 computation.set_node_value(node, NodeValue::Value(HashMap::new()));
             }
