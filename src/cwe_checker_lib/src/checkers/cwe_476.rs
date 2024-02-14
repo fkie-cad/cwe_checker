06fc8253859f0259876e27fb35766c93d75305dc
Pascal Beyer <pascal.beyer@fkie.fraunhofer.de>
2024-02-14 16:05:28 +0100

    Whitespace and comment changes.

--- src/cwe_checker_lib/src/checkers/cwe_476.rs
+++ src/cwe_checker_lib/src/checkers/cwe_476.rs
@@ -47,9 +47,11 @@ use crate::CweModule;
 use petgraph::visit::EdgeRef;
 use std::collections::BTreeMap;
 
+/// Module for the taint tracking state. Reused by the check for CWE-337, hence public.
 pub mod state;
 use state::*;
 
+/// Taint tracking module. Reused by the check for CWE-337, hence public.
 pub mod taint;
 pub use taint::*;
 
