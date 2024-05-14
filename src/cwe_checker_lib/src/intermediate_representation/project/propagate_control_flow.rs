fkie-cad/cwe_checker

commit 2487aac1adeb2ca43d43be099f5217e2d53f878b

remove dead code originating from control flow propagation (#384)

src/cwe_checker_lib/src/intermediate_representation/project/propagate_control_flow.rs
@@ -1,7 +1,8 @@
 use crate::analysis::graph::{Edge, Graph, Node};
 use crate::intermediate_representation::*;
 use petgraph::graph::NodeIndex;
-use std::collections::{BTreeSet, HashMap};
+use petgraph::Direction::Incoming;
+use std::collections::{BTreeSet, HashMap, HashSet};
 
 /// The `propagate_control_flow` normalization pass tries to simplify the representation of
 /// sequences of if-else blocks that all have the same condition
@@ -13,15 +14,17 @@ use std::collections::{BTreeSet, HashMap};
 /// we look for sequences of (conditional) jumps where the final jump target is determined by the source of the first jump
 /// (because we know that the conditionals for all jumps evaluate to the same value along the sequence).
 /// For such a sequence we then retarget the destination of the first jump to the final jump destination of the sequence.
+/// Lastly, the newly bypassed blocks are considered dead code and are removed.
 pub fn propagate_control_flow(project: &mut Project) {
-    let extern_subs = project
+    let extern_subs: HashSet<Tid> = project
         .program
         .term
         .extern_symbols
         .keys()
         .cloned()
         .collect();
-    let cfg = crate::analysis::graph::get_program_cfg(&project.program, extern_subs);
+    let cfg = crate::analysis::graph::get_program_cfg(&project.program, extern_subs.clone());
+    let nodes_without_incoming_edges_at_beginning = get_nodes_without_incoming_edge(&cfg);
 
     let mut jmps_to_retarget = HashMap::new();
     for node in cfg.node_indices() {
@@ -71,6 +74,15 @@ pub fn propagate_control_flow(project: &mut Project) {
         }
     }
     retarget_jumps(project, jmps_to_retarget);
+
+    let cfg = crate::analysis::graph::get_program_cfg(&project.program, extern_subs);
+    let nodes_without_incoming_edges_at_end = get_nodes_without_incoming_edge(&cfg);
+
+    remove_new_orphaned_blocks(
+        project,
+        nodes_without_incoming_edges_at_beginning,
+        nodes_without_incoming_edges_at_end,
+    );
 }
 
 /// Insert the new target TIDs into jump instructions for which a new target was computed.
@@ -242,6 +254,33 @@ fn negate_condition(expr: Expression) -> Expression {
     }
 }
 
+/// Iterates the CFG and returns all node's blocks, that do not have an incoming edge.
+fn get_nodes_without_incoming_edge(cfg: &Graph) -> HashSet<Tid> {
+    let mut nodes_without_incoming_edges = HashSet::new();
+    for node in cfg.node_indices() {
+        if cfg.neighbors_directed(node, Incoming).next().is_none() {
+            nodes_without_incoming_edges.insert(cfg[node].get_block().tid.clone());
+        }
+    }
+    nodes_without_incoming_edges
+}
+
+/// Calculates the difference of the orphaned blocks and removes them from the project.
+fn remove_new_orphaned_blocks(
+    project: &mut Project,
+    orphaned_blocks_before: HashSet<Tid>,
+    orphaned_blocks_after: HashSet<Tid>,
+) {
+    let new_orphan_blocks: HashSet<&Tid> = orphaned_blocks_after
+        .difference(&orphaned_blocks_before)
+        .collect();
+    for sub in project.program.term.subs.values_mut() {
+        sub.term
+            .blocks
+            .retain(|blk| !new_orphan_blocks.contains(&&blk.tid));
+    }
+}
+
 #[cfg(test)]
 pub mod tests {
     use super::*;
@@ -251,7 +290,7 @@ pub mod tests {
     fn mock_condition_block(name: &str, if_target: &str, else_target: &str) -> Term<Blk> {
         let if_jmp = Jmp::CBranch {
             target: Tid::new(if_target),
-            condition: expr!("zero_flag:1"),
+            condition: expr!("ZF:1"),
         };
         let if_jmp = Term {
             tid: Tid::new(name.to_string() + "_jmp_if"),
@@ -275,7 +314,6 @@ pub mod tests {
 
     fn mock_block_with_defs(name: &str, return_target: &str) -> Term<Blk> {
         let def = def![format!("{name}_def: r0:4 = r1:4")];
-
         let jmp = Jmp::Branch(Tid::new(return_target));
         let jmp = Term {
             tid: Tid::new(name.to_string() + "_jmp"),
@@ -318,9 +356,9 @@ pub mod tests {
         let expected_blocks = vec![
             mock_condition_block("cond_blk_1", "def_blk_1", "end_blk"),
             mock_block_with_defs("def_blk_1", "def_blk_2"),
-            mock_condition_block("cond_blk_2", "def_blk_2", "end_blk"),
+            // cond_blk_2 removed, since no incoming edge anymore
             mock_block_with_defs("def_blk_2", "def_blk_3"),
-            mock_condition_block("cond_blk_3", "def_blk_3", "end_blk"),
+            // cond_blk_3 removed, since no incoming edge anymore
             mock_block_with_defs("def_blk_3", "end_blk"),
             mock_block_with_defs("end_blk", "end_blk"),
         ];

commit 33cfdb60b85f4420fab4aa3e9b65403b8ca1c090

lib/ir/project: propagate control flow for call returns
This patch does two things:

1. It allows the re-targeting of jumps for which no known true condition
   is available. Without a known condition, only blocks that consist of
   a single, unconditional jump can be skipped.
2. It allows the re-targeting of call returns in the same way that we
   already do it for unconditional jumps. For calls we never have a
   known condition as side-effects may invalidate any knowledge we have
   after the execution of all DEFs in the block.

Example:

Before the optimization we might have code like this:

  BLK [blk_0040a9c4]
    DEF [instr_0040a9c4_0] ra:4 = 0x40a9cc:4
    JMP [instr_0040a9c4_1] call sub_00403f80 ret blk_0040a9cc
  BLK [blk_0040a9cc]
    JMP [instr_0040a9cc_1] Jump to blk_0040a9d0
  BLK [blk_0040a9d0]
    DEF [instr_0040a9d0_0] a0:4 = ((0x43:4 << 0x10:4) + 0xffffb730:4)
    JMP [instr_0040a9d0_1] Jump to blk_0040a9d4

whereas after the optimization it becomes:

  BLK [blk_0040a9c4]
    DEF [instr_0040a9c4_0] ra:4 = 0x40a9cc:4
    JMP [instr_0040a9c4_1] call sub_00403f80 ret blk_0040a9d0
  BLK [blk_0040a9d0]
    DEF [instr_0040a9d0_0] a0:4 = ((0x43:4 << 0x10:4) + 0xffffb730:4)
    JMP [instr_0040a9d0_1] Jump to blk_0040a9d4

Fixes: 2487aac ("remove dead code originating from control flow propagation (#384)")
Closes: https://github.com/fkie-cad/cwe_checker/issues/461
Reported-by: https://github.com/ElDavoo
Signed-off-by: Valentin Obst <valentin.obst@fkie.fraunhofer.de>

src/cwe_checker_lib/src/intermediate_representation/project/propagate_control_flow.rs
@@ -27,15 +27,50 @@ pub fn propagate_control_flow(project: &mut Project) {
             // Check whether we can propagate the control flow for outgoing jumps
             match &block.term.jmps[..] {
                 [Term {
-                    term: Jmp::Branch(target),
-                    tid: jump_tid,
+                    tid: call_tid,
+                    term:
+                        Jmp::Call {
+                            target: _,
+                            return_: Some(return_target),
+                        },
+                }]
+                | [Term {
+                    tid: call_tid,
+                    term:
+                        Jmp::CallInd {
+                            target: _,
+                            return_: Some(return_target),
+                        },
+                }]
+                | [Term {
+                    tid: call_tid,
+                    term:
+                        Jmp::CallOther {
+                            description: _,
+                            return_: Some(return_target),
+                        },
                 }] => {
-                    if let Some(true_condition) = &known_conditional_result {
-                        if let Some(new_target) =
-                            find_target_for_retargetable_jump(target, &sub.term, true_condition)
-                        {
-                            jmps_to_retarget.insert(jump_tid.clone(), new_target);
-                        }
+                    if let Some(new_target) = find_target_for_retargetable_jump(
+                        return_target,
+                        &sub.term,
+                        // Call may have side-effects that invalidate our
+                        // knowledge about any condition we know to be true
+                        // after execution of all DEFs in a block.
+                        None,
+                    ) {
+                        jmps_to_retarget.insert(call_tid.clone(), new_target);
+                    }
+                }
+                [Term {
+                    tid: jump_tid,
+                    term: Jmp::Branch(target),
+                }] => {
+                    if let Some(new_target) = find_target_for_retargetable_jump(
+                        target,
+                        &sub.term,
+                        known_conditional_result.as_ref(),
+                    ) {
+                        jmps_to_retarget.insert(jump_tid.clone(), new_target);
                     }
                 }
                 [Term {
@@ -50,14 +85,14 @@ pub fn propagate_control_flow(project: &mut Project) {
                     tid: jump_tid_else,
                 }] => {
                     if let Some(new_target) =
-                        find_target_for_retargetable_jump(if_target, &sub.term, condition)
+                        find_target_for_retargetable_jump(if_target, &sub.term, Some(condition))
                     {
                         jmps_to_retarget.insert(jump_tid_if.clone(), new_target);
                     }
                     if let Some(new_target) = find_target_for_retargetable_jump(
                         else_target,
                         &sub.term,
-                        &negate_condition(condition.clone()),
+                        Some(&negate_condition(condition.clone())),
                     ) {
                         jmps_to_retarget.insert(jump_tid_else.clone(), new_target);
                     }
@@ -85,7 +120,20 @@ fn retarget_jumps(project: &mut Project, mut jmps_to_retarget: HashMap<Tid, Tid>
             for jmp in blk.term.jmps.iter_mut() {
                 if let Some(new_target) = jmps_to_retarget.remove(&jmp.tid) {
                     match &mut jmp.term {
-                        Jmp::Branch(target) | Jmp::CBranch { target, .. } => *target = new_target,
+                        Jmp::Branch(target)
+                        | Jmp::CBranch { target, .. }
+                        | Jmp::Call {
+                            target: _,
+                            return_: Some(target),
+                        }
+                        | Jmp::CallInd {
+                            target: _,
+                            return_: Some(target),
+                        }
+                        | Jmp::CallOther {
+                            description: _,
+                            return_: Some(target),
+                        } => *target = new_target,
                         _ => panic!("Unexpected type of jump encountered."),
                     }
                 }
@@ -101,7 +149,7 @@ fn retarget_jumps(project: &mut Project, mut jmps_to_retarget: HashMap<Tid, Tid>
 fn find_target_for_retargetable_jump(
     target: &Tid,
     sub: &Sub,
-    true_condition: &Expression,
+    true_condition: Option<&Expression>,
 ) -> Option<Tid> {
     let mut visited_tids = BTreeSet::from([target.clone()]);
     let mut new_target = target;
@@ -129,27 +177,33 @@ fn find_target_for_retargetable_jump(
 /// If it can be predicted, return the target of the jump.
 fn check_for_retargetable_block<'a>(
     block: &'a Term<Blk>,
-    true_condition: &Expression,
+    true_condition: Option<&Expression>,
 ) -> Option<&'a Tid> {
     if !block.term.defs.is_empty() {
         return None;
     }
-    match &block.term.jmps[..] {
-        [Term {
-            term: Jmp::Branch(target),
-            ..
-        }] => Some(target),
-        [Term {
-            term:
-                Jmp::CBranch {
-                    target: if_target,
-                    condition,
-                },
-            ..
-        }, Term {
-            term: Jmp::Branch(else_target),
-            ..
-        }] => {
+    match (&block.term.jmps[..], true_condition) {
+        (
+            [Term {
+                term: Jmp::Branch(target),
+                ..
+            }],
+            _,
+        ) => Some(target),
+        (
+            [Term {
+                term:
+                    Jmp::CBranch {
+                        target: if_target,
+                        condition,
+                    },
+                ..
+            }, Term {
+                term: Jmp::Branch(else_target),
+                ..
+            }],
+            Some(true_condition),
+        ) => {
             if condition == true_condition {
                 Some(if_target)
             } else if *condition == negate_condition(true_condition.clone()) {